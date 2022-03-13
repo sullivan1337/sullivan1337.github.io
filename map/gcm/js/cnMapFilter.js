b750de0d9a7058b89ddb028abf361ad891aed640
sullivan1337 <33563292+sullivan1337@users.noreply.github.com>
2022-03-13 16:26:48 -0400

    attempt fix2

--- map/gcm/js/cnMapFilter.js
+++ map/gcm/js/cnMapFilter.js
@@ -593,7 +593,7 @@
 		debug.debug(" parseGCalData() calendar data: ",cdata);
 
 		calendarInfo.calendarId = calendarId;
-		calendarInfo.gcTitle = cdata.Title || 'title1 unknown';
+		calendarInfo.gcTitle = cdata.feed.title ? cdata.feed.title['$t'] : 'title2 unknown';
 		calendarInfo.gcTitle.replace(/"/,'&quot;');
 		calendarInfo.desc = cdata.description || '';
 		calendarInfo.gcLink = 'https://www.google.com/calendar/embed?src='+ calendarId;
