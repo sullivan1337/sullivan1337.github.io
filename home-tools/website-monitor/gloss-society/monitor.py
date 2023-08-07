84ded68142165f9af3fb8e28c9716f2ac99375cd
sullivan1337 <33563292+sullivan1337@users.noreply.github.com>
2023-08-07 14:21:20 -0400

    fix and pdf

--- home-tools/website-monitor/gloss-society/monitor.py
+++ home-tools/website-monitor/gloss-society/monitor.py
@@ -2,6 +2,7 @@ import requests
 import time
 import os
 import json
+from datetime import datetime
 
 AUTH_URL = "https://na0.meevo.com/onlinebooking/api/auth/ob?locationid=107231&businesstypeid=6c5a9c8f-524f-4ef4-aaad-ae0c0128aada"
 MONITOR_URL = "https://na0.meevo.com/onlinebooking/api/ob/scanforopenings"
@@ -70,15 +71,38 @@ def get_bearer_token():
     auth_res.raise_for_status()
     token = auth_res.json().get("bearerToken")
     if token:
-        send_webhook("Bearer token received successfully.")
+        send_webhook("Login successful, checking appointment availability.")
     return token
 
 
-def send_webhook(message):
-    data = {"text": message}
-    response = requests.post(WEBHOOK_URL, json=data)
-    response.raise_for_status()
+def send_webhook(text, user_id="U05A7MV49DJ"):
+    # Determine if hyperlink and datetime should be included in the message.
+    if "Available" in text or "Bookable" in text:
+        # Parse and format date and time.
+        start_date = datetime.strptime(DATA["startDate"], "%Y-%m-%dT%H:%M:%S%z").strftime("%-m-%d-%Y")
+        end_date = datetime.strptime(DATA["endDate"], "%Y-%m-%dT%H:%M:%S%z").strftime("%-m-%d-%Y")
+        start_time = datetime.strptime(DATA["startTime"], "%Y-%m-%dT%H:%M:%S%z").strftime("%-I:%M%p").lower()
+        end_time = datetime.strptime(DATA["endTime"], "%Y-%m-%dT%H:%M:%S%z").strftime("%-I:%M%p").lower()
+        formatted_datetime = f"{start_date} {start_time} to {end_date} {end_time}"
+        formatted_datetime = f"{start_date} {start_time} to {end_date} {end_time}"
 
+        text += f"\nSelected Date and Time of appointment: {formatted_datetime}"
+    
+    if "Available" in text:
+        text += f"\n<@{user_id}> <https://na0.meevo.com/OnlineBookingApp/booking/reserve?tenantId=104085|Click here to book now!>"
+
+
+    SLACK_DATA = {
+        "text": text,
+        "username": "Gloss Society"
+    }
+    
+    response = requests.post(WEBHOOK_URL, data=json.dumps(SLACK_DATA),
+                             headers={'Content-Type': 'application/json'})
+    
+    if response.status_code != 200:
+        raise Exception(f"Slack responded with status code {response.status_code}, the error was: {response.text}")
+    
 def monitor_website():
     last_status = None
     while True:
@@ -86,9 +110,9 @@ def monitor_website():
         if token:
             MONITOR_HEADERS['Authorization'] = f'Bearer {token}'
             res = requests.post(MONITOR_URL, headers=MONITOR_HEADERS, json=DATA)
-            new_status = "Available" if res.json() else "Not Available"
+            new_status = "!! Available !!" if res.json() else "NOT Bookable"
             if new_status != last_status:  # if status has changed
-                send_webhook(f"Status changed to {new_status}")
+                send_webhook(f"Nail Appointment availability changed to {new_status}")
                 last_status = new_status  # update the last status
             print("Status code:", res.status_code)
         else:
